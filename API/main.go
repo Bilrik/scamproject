891d0c92cbc4eb0924714557cbb226a511e4396c
Christian Howell <chowell@firstorion.com>
2022-03-02 13:07:23 -0600

    import issues

--- API/main.go
+++ API/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"grpci"
-
 	"github.com/gin-gonic/gin"
 )
 
 func setuproutes(app *gin.Engine) {
 	app.GET("/api/call-protection/calls", grpci.calls)
 	app.GET("/api/call-protection/batch", grpci.batch)
-	app.POST("/api/v1/book", grpci.NewBook)
+	app.POST("/api/v1/book", grpci.update)
 }
 
 func main() {

ee88ad345c28d514ef60759b7ae50efc7cb4e1bb
Christian Howell <chowell@firstorion.com>
2022-03-02 13:01:17 -0600

    api work

--- API/main.go
+++ API/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/Bilrik/scamproject/grpci"
+	"grpci"
 
 	"github.com/gin-gonic/gin"
 )
 
 func setuproutes(app *gin.Engine) {
-	app.GET("/api/v1/book", grpci.GetBooks)
-	app.GET("/api/v1/book/:id", grpci.GetBook)
+	app.GET("/api/call-protection/calls", grpci.calls)
+	app.GET("/api/call-protection/batch", grpci.batch)
 	app.POST("/api/v1/book", grpci.NewBook)
 }
 

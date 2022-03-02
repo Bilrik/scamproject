9c308bf3b5fe6fc795d95a6c56752a552e760f32
Christian Howell <chowell@firstorion.com>
2022-03-02 12:37:53 -0600

    API start of implement

---
+++ API/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/Bilrik/scamproject/API/grpci"
+	"github.com/gin-gonic/gin"
+)
+
+func setuproutes(app *gin.Engine) {
+	app.GET("/api/v1/book", grpci.GetBooks)
+	app.GET("/api/v1/book/:id", grpci.GetBook)
+	app.POST("/api/v1/book", grpci.NewBook)
+}
+
+func main() {
+	app := gin.Default()
+	setuproutes(app)
+}

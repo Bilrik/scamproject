4e411caa62969c612d79f3fb42e911457fdf9af6
Christian Howell <chowell@firstorion.com>
2022-03-04 13:12:45 -0600

    fixed API

--- API/main.go
+++ API/main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
-	"scamProtec/API/grpci"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	app := gin.Default()
-	app.GET("/api/call-protection/batch/:Number", grpci.Batch)
+	app.POST("/api/call-protection/batch", func(c *gin.Context) {
+		ANumber := c.PostForm("ANumber")
+		BNumber := c.PostForm("BNumber")
+
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"ANumber": ANumber,
+			"BNumber": BNumber,
+		})
+	})
+
+	app.POST("/form_post", func(c *gin.Context) {
+
+		test := c.PostForm("message")
+		nick := c.PostForm("nick")
+		fmt.Println(test)
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"message": test,
+			"nick":    nick,
+		})
+	})
+
 	app.Run("localhost:50054")
 }
